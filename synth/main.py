239275c56fdd2aae914414d3b115ebcdd2d2baf7
inixyz <alexandru.ene2702@gmail.com>
2025-04-06 00:32:11 +0300

    100 column limit and basic oscillator

--- synth/main.py
+++ synth/main.py
@@ -1,5 +1,26 @@
+import math
+import itertools
+import matplotlib.pyplot as plt
+
+
+def sin_oscillator(freq, amp=1, phase=0, sr=44100):
+    increment = 2 * math.pi * freq / sr
+    phase_rad = phase * math.pi / 180
+    return (amp * math.sin(x + phase_rad) for x in itertools.count(start=0, step=increment))
+
+
+def plot_signal(signal):
+    plt.plot(signal)
+    plt.ylabel("amplitude")
+    plt.xlabel("sample idx")
+    plt.show()
+
+
 def main():
-    print("Hello, World!")
+    osc = sin_oscillator(1, 512)
+    samples = [next(osc) for _ in range(512)]
+
+    plot_signal(samples)
 
 
 if __name__ == "__main__":
