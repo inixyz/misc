fa93dd79deaf663c61eac12d8ae8efaa48589f0f
inixyz <alexandru.ene2702@gmail.com>
2025-08-21 17:38:42 +0300

    forward

--- mnist_classifier/main.py
+++ mnist_classifier/main.py
@@ -1,10 +1,5 @@
 import numpy as np
 import gzip
-import matplotlib.pyplot as plt
-
-def load_data(path):
-  with open(path, "rb") as f:   
-    return np.frombuffer(gzip.decompress(f.read()), dtype=np.uint8).copy()
 
 X_TRAIN_PATH = "dataset/train-images-idx3-ubyte.gz"
 Y_TRAIN_PATH = "dataset/train-labels-idx1-ubyte.gz"
@@ -12,9 +7,43 @@ Y_TRAIN_PATH = "dataset/train-labels-idx1-ubyte.gz"
 X_TEST_PATH = "dataset/t10k-images-idx3-ubyte.gz"
 Y_TEST_PATH = "dataset/t10k-labels-idx1-ubyte.gz"
 
+def load_data(path):
+  with open(path, "rb") as f:   
+    return np.frombuffer(gzip.decompress(f.read()), dtype=np.uint8).copy()
+
+
+def init_weights():
+  fc1 = np.random.rand(784, 128)
+  b1 = np.random.rand(128)
+  fc2 = np.random.rand(128, 10)
+  b2 = np.random.rand(10)
+  return fc1, b1, fc2, b2
+
+
+def forward(x, fc1, b1, fc2, b2):
+  x = np.maximum(x @ fc1 + b1, 0) # layer1
+  x = np.maximum(x @ fc2 + b2, 0) # layer2
+  return x
+
+
+def backward(loss, fc1, b1, fc2, b2):
+  grad_loss = 1  
+  return grad_fc1, grad_b1, grad_fc2, grad_b2
+
+
 def main():
   x_train = load_data(X_TRAIN_PATH)[16:].reshape(-1, 28, 28)
   y_train = load_data(Y_TRAIN_PATH)[8:]
+
+  batch_size = 16 
+  weights = init_weights()
+
+  x = x_train[:batch_size].reshape(-1, 784)
+  y = np.argmax(forward(x, *weights), axis=1)
+
+  loss = np.mean((y_train[:batch_size] - y) ** 2)
+  print(loss)
+
 
 if __name__ == "__main__":
   main()
