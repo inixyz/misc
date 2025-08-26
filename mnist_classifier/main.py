06572c87260edcae1c7ea153f6133d2a6f814999
inixyz <alexandru.ene2702@gmail.com>
2025-08-26 10:58:49 +0300

    tensor grad

--- mnist_classifier/main.py
+++ mnist_classifier/main.py
@@ -1,49 +1,87 @@
 import numpy as np
 import gzip
 
-X_TRAIN_PATH = "dataset/train-images-idx3-ubyte.gz"
-Y_TRAIN_PATH = "dataset/train-labels-idx1-ubyte.gz"
+# X_TRAIN_PATH = "dataset/train-images-idx3-ubyte.gz"
+# Y_TRAIN_PATH = "dataset/train-labels-idx1-ubyte.gz"
+# 
+# X_TEST_PATH = "dataset/t10k-images-idx3-ubyte.gz"
+# Y_TEST_PATH = "dataset/t10k-labels-idx1-ubyte.gz"
+# 
+# def load_data(path):
+#   with open(path, "rb") as f:   
+#     return np.frombuffer(gzip.decompress(f.read()), dtype=np.uint8).copy()
+# 
+# 
+# def init_weights():
+#   fc1 = np.random.rand(784, 128)
+#   b1 = np.random.rand(128)
+#   fc2 = np.random.rand(128, 10)
+#   b2 = np.random.rand(10)
+#   return fc1, b1, fc2, b2
+# 
+# 
+# def forward(x, fc1, b1, fc2, b2):
+#   x = np.maximum(x @ fc1 + b1, 0) # layer1
+#   x = np.maximum(x @ fc2 + b2, 0) # layer2
+#   return x
+# 
+# 
+# def backward(loss, fc1, b1, fc2, b2):
+#   grad_loss = 1  
+#   return grad_fc1, grad_b1, grad_fc2, grad_b2
+# 
+# 
+# def main():
+#   x_train = load_data(X_TRAIN_PATH)[16:].reshape(-1, 28, 28)
+#   y_train = load_data(Y_TRAIN_PATH)[8:]
+# 
+#   batch_size = 16 
+#   weights = init_weights()
+# 
+#   x = x_train[:batch_size].reshape(-1, 784)
+#   y = np.argmax(forward(x, *weights), axis=1)
+# 
+#   loss = np.mean((y_train[:batch_size] - y) ** 2)
+#   print(loss)
 
-X_TEST_PATH = "dataset/t10k-images-idx3-ubyte.gz"
-Y_TEST_PATH = "dataset/t10k-labels-idx1-ubyte.gz"
+class Tensor:
+  def __init__(self, data, _prev=None):
+    self.data = np.array(data) 
+    self.grad = np.zeros_like(self.data)
+    self._prev = _prev
 
-def load_data(path):
-  with open(path, "rb") as f:   
-    return np.frombuffer(gzip.decompress(f.read()), dtype=np.uint8).copy()
+  def __repr__(self):
+    return f"Tensor(data={self.data}, grad={self.grad})"
 
+  def __add__(self, other):
+    out = Tensor(self.data + other.data, (self, other))
 
-def init_weights():
-  fc1 = np.random.rand(784, 128)
-  b1 = np.random.rand(128)
-  fc2 = np.random.rand(128, 10)
-  b2 = np.random.rand(10)
-  return fc1, b1, fc2, b2
+    def grad_fn():
+      self.grad += out.grad  
+      other.grad += out.grad
 
+    out.grad_fn = grad_fn
 
-def forward(x, fc1, b1, fc2, b2):
-  x = np.maximum(x @ fc1 + b1, 0) # layer1
-  x = np.maximum(x @ fc2 + b2, 0) # layer2
-  return x
+    return out
+  
+  def backward(self):
+    topo = []
+    visited = {}
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
 
-
-def backward(loss, fc1, b1, fc2, b2):
-  grad_loss = 1  
-  return grad_fc1, grad_b1, grad_fc2, grad_b2
-
+    self.grad = np.ones_like(self.grad)
+    for v in reversed(topo):
+      v.grad_fn()
 
 def main():
-  x_train = load_data(X_TRAIN_PATH)[16:].reshape(-1, 28, 28)
-  y_train = load_data(Y_TRAIN_PATH)[8:]
-
-  batch_size = 16 
-  weights = init_weights()
-
-  x = x_train[:batch_size].reshape(-1, 784)
-  y = np.argmax(forward(x, *weights), axis=1)
-
-  loss = np.mean((y_train[:batch_size] - y) ** 2)
-  print(loss)
-
+  x = Tensor([2, 3])
+  print(x)
 
 if __name__ == "__main__":
   main()
